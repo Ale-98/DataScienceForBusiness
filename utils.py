268c5721a63eed77bc7fd37722aa9ea8713a759f
Andrea Nonali <nonali.andrea@gmail.com>
2022-03-18 16:07:03 +0100

    add plot for frequency table

--- utils.py
+++ utils.py
@@ -32,9 +32,9 @@ def get_percent_missing(df: pd.DataFrame, columns=None):
     return percent_missing[columns]
 
 
-def trimmed_mean(df: pd.DataFrame, column: str, limit: int):
+def trimmed_mean(df: pd.DataFrame, column: str, limit: float):
     '''Calculates the trimmed mean of the given column and with the given limit'''
-    col_sorted = df[column].sort_values()
+    col_sorted = df.column.sort_values()
     col_sorted = col_sorted.reset_index(drop=True, inplace=False)
     upper_limit = col_sorted.quantile(1-limit)
     lower_limit = col_sorted.quantile(limit)
@@ -45,36 +45,39 @@ def trimmed_mean(df: pd.DataFrame, column: str, limit: int):
 
 def mean_absolute_deviation(df: pd.DataFrame, column: str):
     '''Calculates the mean absolute deviation of the given column'''
-    mean = df[column].mean()
-    num = np.sum(np.abs(df[column].values - mean)) / len(df[column])
+    mean = df.column.mean()
+    num = np.sum(np.abs(df.column.values - mean)) / len(df.column)
     return num
 
 
 def median_absolute_deviation(df: pd.DataFrame, column: str):
     '''Calculates the median absolute deviation of the given column'''
-    median = df[column].median()
-    num = np.sum(np.abs(df[column].values - median)) / len(df[column])
+    median = df.column.median()
+    num = np.sum(np.abs(df.column.values - median)) / len(df.column)
     return num
 
 
-def variability_measures(df: pd.DataFrame, column: str, limit=None):
+def variability_measures(df: pd.DataFrame, column: str, limit: float):
     '''Calculates some measures of variability on the given column of the given pandas dataframe'''
-    print(f'Mean on column {column}: {df[column].mean()}')
-    print(f'Median on column {column}: {df[column].median()}')
-    print(f'Mode on column {column}: {df[column].mode()}')
-    print(f'Variance on column {column}: {df[column].var()}')
-    print(f'Standard deviation on column {column}: {df[column].std()}')
-    if limit is not None:
-        print(f'Trimmed mean on column {column} with limit {limit}: {trimmed_mean(df, column, limit)}')
+    print(f'Mean on column {column}: {df.column.mean()}')
+    print(f'Median on column {column}: {df.column.median()}')
+    print(f'Mode on column {column}: {df.column.mode()}')
+    print(f'Variance on column {column}: {df.column.var()}')
+    print(f'Standard deviation on column {column}: {df.column.std()}')
+    print(f'Trimmed mean on column {column} with limit {limit}: {trimmed_mean(df, column, limit)}')
     print(f'Mean absolute deviation on column {column}: {mean_absolute_deviation(df, column)}')
     print(f'Median absolute deviation on column {column}: {median_absolute_deviation(df, column)}')
 
-def plot_frequency_pie(df: pd.DataFrame, column: str):
+
+def plot_frequency_table(df: pd.DataFrame, column: str):
     freq_table = df[column].value_counts()
-    ax2 = freq_table.plot.pie(
+    freq_table.plot.barh(
         x=freq_table.index,
         y=freq_table.values,
     )
+
+def plot_frequency_pie(df: pd.DataFrame, column: str):
+    freq_table = df[column].value_counts()
     colors = sns.color_palette('pastel')[0:5]
     plt.pie(freq_table.values, labels=freq_table.index, colors=colors, autopct='%.0f%%')
     plt.show()
